9944fd16bc5351e4386a8a55062be9d507923c09
Quatre.Finance <35783747+bobeu@users.noreply.github.com>
2022-09-10 15:55:39 +0100

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,144 +1,3 @@
-// const { parseEther } = require("ethers/lib/utils");
-// const { extendEnvironment } = require("hardhat/config");
-
-// require("dotenv").config();
-// require("@nomiclabs/hardhat-waffle");
-// require("hardhat-gas-reporter");
-// require("@nomiclabs/hardhat-etherscan");
-// require('hardhat-contract-sizer');
-// require('@openzeppelin/hardhat-upgrades');
-// require('solidity-docgen');
-
-// const networkVariables = require('./networkVariables');
-
-// // Fill networkVariables object with settings and addresses based on current network or fork.
-// extendEnvironment((hre) => {
-//   if(hre.network.name == 'hardhat') {
-//     if(hre.network.config.forking.enabled) {
-//       switch (hre.network.config.forking.url) {
-//         case process.env.BNB_URL:
-//           // console.log(networkVariables);
-//           hre.networkVariables = networkVariables['bnb'];
-//           break;
-//         case process.env.BNB_TEST_URL:
-//           // console.log(networkVariables);
-//           hre.networkVariables = networkVariables['bnbTest'];
-//           break;
-//       }
-//     }
-//   } else {
-//     hre.networkVariables = networkVariables[hre.network.name];
-//   }
-//   if(!hre.networkVariables) throw Error("network variables are missing");
-//   // console.log(hre.networkVariables);
-// });
-
-// /**
-//  * @type import('hardhat/config').HardhatUserConfig
-//  */
-// module.exports = {
-//   networks: {
-//     hardhat: {
-//       forking: {
-//         url: process.env.BNB_URL,
-//         // blockNumber: 19232650, // use this only with archival node
-//         enabled: true
-//       },
-//       // allowUnlimitedContractSize: true,
-//       // loggingEnabled: false
-//       // accounts: [{privateKey: process.env.PRIVATE_KEY, balance: parseEther("10000").toString()}],
-//     },
-//     bnb: {
-//       url: `${process.env.BNB_URL}`,
-//       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-//       gas: 20e6 // lets see if this solves problem, as auto gas estimation makes deploy scripts to fail
-//     },
-//     bnbTest: {
-//       url: process.env.BNB_TEST_URL ?? '',
-//       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-//       gas: 20e6 // lets see if this solves problem, as auto gas estimation makes deploy scripts to fail
-//     },
-//   },
-//   gasReporter: {
-//     enabled: process.env.REPORT_GAS !== undefined,
-//   },
-//   mocha: {
-//     bail: true,
-//     timeout: 6000000
-//   },
-//   etherscan: {
-//     apiKey: {
-//       bsc: process.env.BSCSCAN_API_KEY
-//     }
-//   },
-//   docgen: {
-//     pages: "files"
-//   },
-//   solidity: {
-//     compilers: [
-//       {
-//         version: "0.8.4",
-//         settings: {
-//           optimizer: {
-//             enabled: true,
-//             runs: 200,
-//           },
-//           evmVersion: "istanbul",
-//           outputSelection: {
-//             "*": {
-//               "": ["ast"],
-//               "*": [
-//                 "evm.bytecode.object",
-//                 "evm.deployedBytecode.object",
-//                 "abi",
-//                 "evm.bytecode.sourceMap",
-//                 "evm.deployedBytecode.sourceMap",
-//                 "metadata",
-//               ],
-//             },
-//           },
-//         },
-//       },
-//       {
-//         version: "0.6.6",
-//         settings: {
-//           optimizer: {
-//             enabled: true,
-//             runs: 200,
-//           },
-//           evmVersion: "istanbul",
-//           outputSelection: {
-//             "*": {
-//               "": ["ast"],
-//               "*": [
-//                 "evm.bytecode.object",
-//                 "evm.deployedBytecode.object",
-//                 "abi",
-//                 "evm.bytecode.sourceMap",
-//                 "evm.deployedBytecode.sourceMap",
-//                 "metadata",
-//               ],
-//             },
-//           },
-//         },
-//       },
-//     ]
-//   },
-// };
-
-
-
-
-
-// import { config } from "dotenv";
-// import type { HardhatUserConfig } from "hardhat/types";
-// import "@nomiclabs/hardhat-ethers";
-// import "@openzeppelin/hardhat-upgrades";
-// import "@nomiclabs/hardhat-waffle";
-// import "@nomiclabs/hardhat-truffle5";
-// import "@nomiclabs/hardhat-web3";
-// import "solidity-coverage";
-
 require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
@@ -168,17 +27,17 @@ module.exports = {
     // },
     hardhat: {
     },
-    development: {
-      url: "http://127.0.0.1:8545",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-    },
+    // development: {
+    //   url: "http://127.0.0.1:8545",     // Localhost (default: none)
+    //   port: 8545,            // Standard Ethereum port (default: none)
+    //   network_id: "*",       // Any network (default: none)
+    // },
 
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+    // ropsten: {
+    //   url: process.env.ROPSTEN_URL || "",
+    //   accounts:
+    //     process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    // },
 
     // rinkeby: {
     //   url: process.env.RINKEBY_URL || "",
@@ -190,15 +49,15 @@ module.exports = {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: {mnemonic: process.env.MNEMONIC}
+      accounts: {mnemonic: process.env.MNEMONICPHRASE}
     },
 
-    mainnet: {
-      url: "https://bsc-dataseed.binance.org/",
-      chainId: 56,
-      gasPrice: 20000000000,
-      accounts: {mnemonic: process.env.MNEMONIC}
-    },
+    // mainnet: {
+    //   url: "https://bsc-dataseed.binance.org/",
+    //   chainId: 56,
+    //   gasPrice: 20000000000,
+    //   accounts: {mnemonic: process.env.MNEMONIC}
+    // },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,7 +15,7 @@ async function main() {
     const [deployer] = await hre.ethers.getSigners();
     console.log(`Deploying with the account:`, deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
-    const StrategyRouterLib =await ethers.getContractFactory("contracts/verify/StrategyRouter:StrategyRouterLib");
+    const StrategyRouterLib =await ethers.getContractFactory("contracts/verify/StrategyRouterOneFile.sol:StrategyRouterLib");
     const strategyRouterLib = await StrategyRouterLib.deploy();
     strategyRouterLib.deployed();
     const libAddress = strategyRouterLib.address;
@@ -28,49 +28,64 @@ async function main() {
         },
       }
     );
+    // StrategyRouter
     const strategyRouter = await StrategyRouter.deploy();
     await strategyRouter.deployed();
     const strategyAddress = strategyRouter.address;
+    console.log("strategyAddress : deployed to", strategyAddress ); 
 
+    // SharesToken
+    const ShareToken =await ethers.getContractFactory("SharesTokenOnefile");
+    const shareToken = await ShareToken.deploy(strategyAddress);
+    await shareToken.deployed();
+    const shareTokenAddr = shareToken.address;
+    console.log("shareTokenAddr : deployed to", shareTokenAddr ); 
 
-    const ShareToken =await ethers.getContractFactory("ShareTokenOnefile");
+    // ChainLinkOracle
     const ChainlinkOracle =await ethers.getContractFactory("ChainlinkOracleOnefile");
+    const chainlinkOracle = await ChainlinkOracle.deploy();
+    await chainlinkOracle.deployed();
+    const chainlinkAddr = chainlinkOracle.address;
+    console.log("chainlinkAddrgy : deployed to", chainlinkAddr ); 
+
+    // Batch
     const Batch = await ethers.getContractFactory("BatchOnefile");
+    const batch = await Batch.deploy();
+    await batch.deployed();
+    const batchAddr = batch.address;
+    console.log("batchAddr : deployed to", batchAddr ); 
+
+    // Exchange
     const Exchange =await ethers.getContractFactory("ExchangeOnefile");
+    const exchange = await Exchange.deploy();
+    await exchange.deployed();
+    const exchangeAddr = exchange.address;
+    console.log("exchangeAddr : deployed to", exchangeAddr ); 
+
+    // ReceiptNFT
     const ReceiptNFT =await ethers.getContractFactory("ReceiptNFTOnefile");
+    const receiptNFT = await ReceiptNFT.deploy(strategyAddress, batchAddress);
+    await receiptNFT.deployed();
+    const receiptNFTAddr = receiptNFT.address;
+    console.log("receiptNFTAddr : deployed to", receiptNFTAddr );
+
+    // DodoStrategy
     const DodoStrategy =await ethers.getContractFactory("DodoStrategyOnefile");
+    const dodoStrategy = await DodoStrategy.deploy();
+    await dodoStrategy.deployed();
+    const dodoAddr = dodoStrategy.address;
+    console.log("dodoAddr : deployed to", dodoAddr ); 
+
+
    
     // const strategyRouter = await StrategyRouter.deploy();
-    const dodoStrategy = await DodoStrategy.deploy();
-    const shareToken = await ShareToken.deploy(strategyAddress, strategyAddress);
-    const chainlinkOracle = await ChainlinkOracle.deploy();
-    const batch = await Batch.deploy();
-    const exchange = await Exchange.deploy();
-    const receiptNFT = await ReceiptNFT.deploy(batchAddress, );
-    await receiptNFT.deployed();
+    
 
-    await dodoStrategy.deployed();
-    await shareToken.deployed();
-    await chainlinkOracle.deployed();
-    await batch.deployed();
-    await exchange.deployed();
 
-    // const address = strategyRouter.address;
-    const dodoAddr = dodoStrategy.address;
-    const shareTokenAddr = shareToken.address;
-    const chainlinkAddr = chainlinkOracle.address;
-    const batchAddr = batch.address;
-    const exchangeAddr = exchangey.address;
-    const receiptNFTAddr = receiptNFT.address;
 
     // console.log("strategyRouter : deployed to", address ); 
-    console.log("strategyAddress : deployed to", strategyAddress ); 
-    console.log("dodoAddr : deployed to", dodoAddr ); 
-    console.log("shareTokenAddr : deployed to", shareTokenAddr ); 
-    console.log("chainlinkAddrgy : deployed to", chainlinkAddr ); 
-    console.log("batchAddr : deployed to", batchAddr ); 
-    console.log("exchangeAddr : deployed to", exchangeAddr ); 
-    console.log("receiptNFTAddr : deployed to", receiptNFTAddr );
+    
+    
 }
   
   // We recommend this pattern to be able to use async/await everywhere
